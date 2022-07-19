74927681d59db9b0b3cfe1632aeb4492cf95cea7
akif-onder <100137858+akif-onder@users.noreply.github.com>
2022-07-19 22:00:29 +0300

    login page completed...

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,11 +1,59 @@
-import React from 'react';
+import React, { useState } from "react";
 
-const Login = () => {
+const Register = () => {
+  const [email, setEmail] = useState();
+  const [password, setPassword] = useState();
+
+  const handleLogin = (e) => {
+    e.preventDefault();
+    console.log(email, password);
+  };
+
   return (
-    <div>
-      <p>Login</p>
+    <div className="d-flex justify-content-center">
+      <div className="form-image d-none d-md-block ">
+        <img src={"https://picsum.photos/800/800"} alt="sample-movie" />
+      </div>
+      <div className="register-form">
+        <h1 className="form-title display-3 ">Login</h1>
+        <form id="register" onSubmit={handleLogin}>
+          <div className="mb-3">
+            <label htmlFor="email" className="form-label">
+              Email
+            </label>
+            <input
+              type="email"
+              className="form-control"
+              id="email"
+              placeholder="Enter your email adress.."
+              required
+              onChange={(e) => setEmail(e.target.value)}
+            />
+          </div>
+          <div className="mb-3">
+            <label htmlFor="password" className="form-label">
+              Password
+            </label>
+            <input
+              type="password"
+              className="form-control"
+              id="password"
+              placeholder="Enter your password.."
+              required
+              onChange={(e) => setPassword(e.target.value)}
+            />
+          </div>
+          <div className="link">Forgot Password?</div>
+          <input
+            type="submit"
+            className="btn btn-primary form-control"
+            value="Login"
+          />
+        </form>
+        <div className="btn btn-primary form-control">Continue With Google</div>
+      </div>
     </div>
   );
 };
 
-export default Login;
+export default Register;
