df2b965373b568a3c3a32d9f1e3819aa7c2f38f7
akif-onder <100137858+akif-onder@users.noreply.github.com>
2022-07-19 21:36:10 +0300

    register completed

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,14 +1,24 @@
-import React from 'react';
+import React, { useState } from "react";
 
 const Register = () => {
+  const [firstName, setFirstName] = useState();
+  const [lastName, setLastName] = useState();
+  const [email, setEmail] = useState();
+  const [password, setPassword] = useState();
+
+  const handleSubmit = (e)=>{
+    e.preventDefault();
+    console.log(firstName, lastName);
+  }
+
   return (
     <div className="d-flex justify-content-center">
       <div className="form-image d-none d-md-block ">
-        <img src={'https://picsum.photos/800/800'} alt="sample-movie" />
+        <img src={"https://picsum.photos/800/800"} alt="sample-movie" />
       </div>
       <div className="register-form">
         <h1 className="form-title display-3 ">Register</h1>
-        <form id="register">
+        <form id="register" onSubmit={handleSubmit}>
           <div className="mb-3">
             <label htmlFor="firstName" className="form-label">
               First Name
@@ -19,6 +29,7 @@ const Register = () => {
               id="firstName"
               placeholder="Enter your first name.."
               required
+              onChange={(e) => setFirstName(e.target.value)}
             />
           </div>
           <div className="mb-3">
@@ -31,6 +42,7 @@ const Register = () => {
               id="lastName"
               placeholder="Enter your last name.."
               required
+              onChange={(e) => setLastName(e.target.value)}
             />
           </div>
           <div className="mb-3">
@@ -43,6 +55,7 @@ const Register = () => {
               id="email"
               placeholder="Enter your email adress.."
               required
+              onChange={(e) => setEmail(e.target.value)}
             />
           </div>
           <div className="mb-3">
@@ -55,6 +68,7 @@ const Register = () => {
               id="password"
               placeholder="Enter your password.."
               required
+              onChange={(e) => setPassword(e.target.value)}
             />
           </div>
           <input
