cb181897833e59035f7345184873f0c594eab3ac
akif-onder <100137858+akif-onder@users.noreply.github.com>
2022-07-19 22:21:28 +0300

    .env added to firebase.js

--- src/auth/firebase.js
+++ src/auth/firebase.js
@@ -1,21 +1,15 @@
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
-// TODO: Replace the following with your app's Firebase project configuration
-// See: https://firebase.google.com/docs/web/learn-more#config-object
 const firebaseConfig = {
-    apiKey: "AIzaSyBzZip4hWXLJFTxkziex31ooHn0tbclszM",
-    authDomain: "movie-app-akif.firebaseapp.com",
-    projectId: "movie-app-akif",
-    storageBucket: "movie-app-akif.appspot.com",
-    messagingSenderId: "873602438086",
-    appId: "1:873602438086:web:afc6733b56b1ae1a8b58c7"
+  apiKey: process.env.REACT_APP_apiKey,
+  authDomain: process.env.REACT_APP_authDomain,
+  projectId: process.env.REACT_APP_projectId,
+  storageBucket: process.env.REACT_APP_storageBucket,
+  messagingSenderId: process.env.REACT_APP_messagingSenderId,
+  appId: process.env.REACT_APP_appId,
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
 
-
-// Initialize Firebase Authentication and get a reference to the service
 const auth = getAuth(app);
-
