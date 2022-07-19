acdf546dc629d1c0839754231773df51d978beb1
akif-onder <100137858+akif-onder@users.noreply.github.com>
2022-07-19 21:29:10 +0300

    comps and styles

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,33 @@
-import React from 'react';
+import React from "react";
+import {Link,useNavigate} from 'react-router-dom';
 
 const Navbar = () => {
+  const navigate = useNavigate();
+  // const currentUser = {displayName: 'akif onder'};
+  const currentUser = false;
   return (
     <div>
-      <p>Navbar</p>
+      <nav className="navbar navbar-expand-lg">
+        <div className="container-fluid">
+          <Link to={"/"} className="navbar-brand text-white">
+            <h4>React Movie App</h4>
+          </Link>
+          <div className="d-flex text-white align-items-center">
+            {currentUser ? (
+              <>
+              <h5 className="mb-0 text-capitalize" onClick={()=>navigate('/login')}>{currentUser.displayName}</h5>
+              <button className="ms-2 btn-outline">Logout</button>
+              </>
+            ) :(
+              <>
+              <button className="ms-2 btn btn-outline-light">Login</button>
+                <button className="ms-2 btn btn-outline-light">Register</button>
+              </>
+            ) }
+          </div>
+        
+        </div>
+      </nav>
     </div>
   );
 };
