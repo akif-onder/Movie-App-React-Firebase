b35980b768c176b21f91d644de4d364c934bfccb
akif-onder <100137858+akif-onder@users.noreply.github.com>
2022-07-20 23:00:24 +0300

    movie detail comp added

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,24 +1,28 @@
-import React from 'react';
-import { useContext } from 'react';
-import { AuthContext } from '../context/AuthContext';
+import React from "react";
+import { useContext } from "react";
+import { useNavigate } from "react-router-dom";
+import { AuthContext } from "../context/AuthContext";
 
-const IMG_API = 'https://image.tmdb.org/t/p/w1280';
+const IMG_API = "https://image.tmdb.org/t/p/w1280";
 const defaultImage =
-  'https://images.unsplash.com/photo-1581905764498-f1b60bae941a?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=700&q=80';
+  "https://images.unsplash.com/photo-1581905764498-f1b60bae941a?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=700&q=80";
 
 const MovieCard = ({ poster_path, title, overview, vote_average, id }) => {
   const { currentUser } = useContext(AuthContext);
+  const navigate = useNavigate();
   const setVoteClass = (vote) => {
     if (vote > 8) {
-      return 'green';
+      return "green";
     } else if (vote >= 6) {
-      return 'orange';
+      return "orange";
     } else {
-      return 'red';
+      return "red";
     }
   };
   return (
-    <div className="movie">
+    <div className="movie" onClick={() =>{ navigate("/details/" + id);
+    !currentUser && alert('Please log in to see details..')
+    }}>
       <img
         loading="lazy"
         src={poster_path ? IMG_API + poster_path : defaultImage}
