39edb9ea384274a5da3b92066eb58c2d0f1eef89
akif-onder <100137858+akif-onder@users.noreply.github.com>
2022-07-20 21:56:15 +0300

    firebase authentication handled

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,34 +1,53 @@
-import React from "react";
-import { useContext } from "react";
-import {Link,useNavigate} from 'react-router-dom';
-import { AuthContext } from "../context/AuthContext";
+import React from 'react';
+import { useContext } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
+import { logOut } from '../auth/firebase';
+import { AuthContext } from '../context/AuthContext';
 
 const Navbar = () => {
   const navigate = useNavigate();
-  const {currentUser} = useContext(AuthContext)
-  // const currentUser = {displayName: 'akif onder'};
-  
+  const { currentUser } = useContext(AuthContext);
+  //* with custom hook
+  // const { currentUser } = useAuthContext();
+  // const currentUser = { displayName: 'felix franko' };
+  // const currentUser = false;
   return (
     <div>
-      <nav className="navbar navbar-expand-lg">
+      <nav className="navbar navbar-expand-lg ">
         <div className="container-fluid">
-          <Link to={"/"} className="navbar-brand text-white">
+          <Link to="/" className="navbar-brand text-white">
             <h4>React Movie App</h4>
           </Link>
-          <div className="d-flex text-white align-items-center">
+          <div className="d-flex text-white align-items-center ">
             {currentUser ? (
               <>
-              <h5 className="mb-0 text-capitalize" onClick={()=>navigate('/login')}>{currentUser.displayName}</h5>
-              <button className="ms-2 btn-outline">Logout</button>
+                <h5 className="mb-0 text-capitalize">
+                  {currentUser.displayName}
+                </h5>
+                <button
+                  className="ms-2 btn btn-outline-light"
+                  onClick={() => logOut()}
+                >
+                  Logout
+                </button>
               </>
-            ) :(
+            ) : (
               <>
-              <button className="ms-2 btn btn-outline-light">Login</button>
-                <button className="ms-2 btn btn-outline-light">Register</button>
+                <button
+                  className="ms-2 btn btn-outline-light"
+                  onClick={() => navigate('/login')}
+                >
+                  Login
+                </button>
+                <button
+                  className="ms-2 btn btn-outline-light"
+                  onClick={() => navigate('/register')}
+                >
+                  Register
+                </button>
               </>
-            ) }
+            )}
           </div>
-        
         </div>
       </nav>
     </div>
